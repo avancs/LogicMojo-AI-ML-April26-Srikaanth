5527eb52e747f3f4f73a57e3c721ac5eed0eaab0
Srikanth <101379476+avancs@users.noreply.github.com>
2026-05-01 01:18:28 -0400

    Delete print_count_vowels.py

--- print_count_vowels.py
+++
@@ -1,7 +0,0 @@
-str = input("Enter string to count # of vowels:")
-vc = 0
-for vow in str:
-  if vow == "a" or vow == "e" or vow == "i" or vow == "o" or vow == "u":
-    vc = vc + 1
-    print("Vowel in the given string:",vow)
-print("# of vowels in the string are:", vc)
